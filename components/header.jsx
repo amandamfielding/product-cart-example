c6441e78def4d06c6ed5a998535bcfc2fdf6b580
Amanda Fielding <amandafielding@Amandas-MacBook-Pro.local>
2017-01-07 16:57:38 -0800

    add font

--- components/header.jsx
+++ components/header.jsx
@@ -4,7 +4,7 @@ class Header extends React.Component {
   render()  {
     return (
       <div className="header-container">
-        <div>FIJI Water Accessories</div>
+        <div className="logo">FIJI Water Accessories</div>
       </div>
     );
   }
